1560c16067dde8cbf98d6d23ef9756a6b4353d5f
novusr <radjaxyz09@gmail.com>
2026-02-25 03:19:49 +0800

    build: update application versionCode to 5 and versionName to 1.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         applicationId = "com.rvdjv.pawnmc"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.2.8"
+        versionCode = 5
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
