32e08821f6428cafd6ac0578f812151f9f339f80
machijine <machijine@google.com>
2026-09-26 14:19:40 +0700

    v1.4.0 -> v1.5.1 (fire)

--- app/src/test/java/com/rvdjv/pawnmc/data/update/AppUpdateManagerTest.kt
+++ app/src/test/java/com/rvdjv/pawnmc/data/update/AppUpdateManagerTest.kt
@@ -6,7 +6,7 @@ import org.junit.Test
 class AppUpdateManagerTest {
     @Test
     fun compareVersions_handlesGitHubVersionFormats() {
-        assertTrue(AppUpdateManager.compareVersions("1.5.0", "1.4.2") > 0)
+        assertTrue(AppUpdateManager.compareVersions("1.5.1", "1.4.2") > 0)
         assertTrue(AppUpdateManager.compareVersions("v1.4.0", "1.4.0") == 0)
         assertTrue(AppUpdateManager.compareVersions("1.4.1", "1.4.0") > 0)
         assertTrue(AppUpdateManager.compareVersions("1.4.0-beta", "1.4.0") < 0)
